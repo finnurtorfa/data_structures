7d283bbc4ecdbae17465c41d0f18a65877252439
Finnur Torfason <finnurtorfa@gmail.com>
2014-03-15 16:41:14 +0100

    Add barebones files linked_list.{c,h}

---
+++ C/list/linked_list.c
@@ -0,0 +1,6 @@
+/*
+ * File: linked_list.c
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ */
+
---
+++ C/list/linked_list.h
@@ -0,0 +1,11 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+/*
+ * File: linked_list.h
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ * 
+ * Simple linked list implementation
+ */
+
+#endif
