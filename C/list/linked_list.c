67a33bc48f2a6b43862a02553621de5e01ff2697
Finnur Torfason <finnurtorfa@gmail.com>
2014-03-15 22:00:20 +0100

    Add functions for adding, appending and prepending nodes to a list

--- C/list/linked_list.c
+++ C/list/linked_list.c
@@ -4,3 +4,17 @@
  * Compiler: GCC 4.8.2
  */
 
+#include "list/linked_list.h"
+
+static void init_list_node(struct node_t *list) {
+  list->next = list;
+  list->prev = list;
+}
+
+static void list_append(struct node_t *new, struct node_t *old) {
+  __list_add(new, old, old->next);
+}
+
+static void list_prepend(struct node_t *new, struct node_t *old) {
+  __list_add(new, old->prev, old);
+}
--- C/list/linked_list.h
+++ C/list/linked_list.h
@@ -10,8 +10,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * A macro that will declare and initialize a list node
+ */
+#define LIST_NODE(name) \
+  struct node_t name = { &(name), &(name) }
+
 struct node_t {
   struct node_t *next, *prev;
 };
+
+/**
+ * Initialize the list
+ *
+ * @list: A node, which will be initialized
+ */
+static void init_list_node(struct node_t *list);
+
+/**
+ * Insert a new node between two known consecutive nodes in the list.
+ * This function is only used internally.
+ *
+ * @new:  The new node to insert
+ * @prev: The new node goes after this one
+ * @next: The new node goes before this one 
+ */
+static inline void __list_add(struct node_t *new, 
+                              struct node_t *prev,
+                              struct node_t *next) {
+  next->prev = new;
+  new->next  = next;
+  new->prev  = prev;
+  prev->next = new;
+}
+
+/**
+ * Append a new node to an old node. Good for implementing stacks.
+ *
+ * @new: The new node to insert
+ * @old: The old node, which the new node will be appended to
+ */ 
+static void list_append(struct node_t *new, struct node_t *old);
+
+/**
+ * Prepend a new node to an old node. Good for implementing queues.
+ *
+ * @new: The new node to insert
+ * @old: The old node to prepend the new one to.
+ */
+static void list_prepend(struct node_t *new, struct node_t *old);
 
 #endif
