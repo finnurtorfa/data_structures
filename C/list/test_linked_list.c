3f01c80fe2dc94dfd7ff773531a246ef65d10709
Finnur Torfason <finnurtorfa@gmail.com>
2014-03-23 08:33:38 +0100

    Augment linked list test

--- C/list/test_linked_list.c
+++ C/list/test_linked_list.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
   struct linked_list_t my_list;
   LIST_NODE_INIT(&my_list.node);
   
-  printf("Adding items to the linked list\n");
+  printf("Appending items to the linked list\n");
   for (int i = 0; i < 5; i++) {
     tmp = (struct linked_list_t *) 
            malloc(sizeof(struct linked_list_t));
@@ -27,13 +27,27 @@ int main(int argc, char **argv) {
     list_append(&(tmp->node), &(my_list.node));
   }
 
+  printf("Prepending items to the linked list\n");
+  for (int i = 4; i >= 0; i--) {
+    tmp = (struct linked_list_t *) 
+           malloc(sizeof(struct linked_list_t));
+
+    tmp->item = i;
+
+    list_prepend(&(tmp->node), &(my_list.node));
+  }
+
   /* Traverse the list safely and delete every item in the list */
-  for (pos = (&my_list.node)->next, q = pos->next; 
-       pos != (&my_list.node); 
-       pos = q, q = pos->next) {
+  printf("Looping through the list in forward order\n");
+  for_each(pos, q, &my_list.node) {
     tmp = get_list_node(pos, struct linked_list_t, node);
     printf("item: %d\n", tmp->item);
-    printf("Deleting item: %d\n", tmp->item);
+  }
+  
+  printf("Looping through the list in reverse order and deleting the elements\n");
+  for_each_reverse(pos, q, &my_list.node) {
+    tmp = get_list_node(pos, struct linked_list_t, node);
+    printf("item: %d\n", tmp->item);
     list_remove(pos);
     free(tmp);
   }
