43d3baddf41017fb58bb9f552474c668cf006bd7
Finnur Torfason <finnurtorfa@gmail.com>
2014-04-05 09:54:33 +0200

    Add a barebones header and source file for a hashmap

---
+++ C/list/hashmap.c
@@ -0,0 +1,5 @@
+/*
+ * File: hashmap.c
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ */
---
+++ C/list/hashmap.h
@@ -0,0 +1,7 @@
+/*
+ * File: hashmap.h
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ *
+ * A basic implementation of a hashmap
+ */
