aa2652afba6a32795f4ad1e8c71cb11663330138
Finnur Torfason <finnurtorfa@gmail.com>
2014-04-08 21:26:21 +0200

    Structs for a linked list, in case of collisions and the hashmap

--- C/list/hashmap.h
+++ C/list/hashmap.h
@@ -1,3 +1,4 @@
+#pragma once
 /*
  * File: hashmap.h
  * Author: Finnur Smári Torfason
@@ -5,3 +6,19 @@
  *
  * A basic implementation of a hashmap
  */
+
+#include "linked_list.h"
+#include "dynamic_array.h"
+
+struct item_t {
+  char *key;
+  void *item;
+  struct node_t node;
+}
+
+struct hashmap_t {
+  struct item_t **buckets;
+  struct d_array_t items;
+  int size;
+  int num_buckets;
+}
