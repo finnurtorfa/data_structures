27ff4ec7d441d08635c73130a9209fe59b3374de
Finnur Torfason <finnurtorfa@gmail.com>
2014-05-09 22:29:39 +0200

    Add a function for initializing the hashmap

--- C/list/hashmap.c
+++ C/list/hashmap.c
@@ -3,3 +3,17 @@
  * Author: Finnur Smári Torfason
  * Compiler: GCC 4.8.2
  */
+
+#include "hashmap.h"
+
+void init_hashmap(struct hashmap_t *map) {
+  map->size = 0;
+  map->num_buckets = DEFAULT_NUM_BUCKETS;
+  map->buckets = (struct item_t **) malloc(sizeof(struct item_t *) * map->num_buckets);
+
+  for ( int i = 0; i < map->num_buckets; i ++ ) {
+    LIST_NODE_INIT(&(map->buckets[i])->node);
+  }
+
+  d_array_init(&map->items);
+}
--- C/list/hashmap.h
+++ C/list/hashmap.h
@@ -10,15 +10,24 @@
 #include "linked_list.h"
 #include "dynamic_array.h"
 
+#define DEFAULT_NUM_BUCKETS 100
+
 struct item_t {
   char *key;
   void *item;
   struct node_t node;
-}
+};
 
 struct hashmap_t {
   struct item_t **buckets;
   struct d_array_t items;
   int size;
   int num_buckets;
-}
+};
+
+/**
+ * Initialize the hashmap
+ *
+ * @map: A hashmap
+ */
+void init_hashmap(struct hashmap_t *map);
