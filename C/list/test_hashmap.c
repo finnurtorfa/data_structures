1a3b27182e9402186f4ff058d7037e19540e0d58
Finnur Torfason <finnurtorfa@gmail.com>
2014-05-19 10:38:24 +0200

    Test the hashmap

---
+++ C/list/test_hashmap.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hashmap.h"
+
+int main(int argc, char **argv) {
+  struct hashmap_t *map = (struct hashmap_t *)malloc(sizeof(struct hashmap_t));
+
+  char *res;
+
+  printf("Starting!\n");
+
+  hashmap_init(map);
+
+  printf("Hashmap size: %d\n", map->size);
+
+  hashmap_insert(map, "key", (void *)"value", 6);
+  printf("Hashmap size: %d\n", map->size);
+
+  hashmap_insert(map, "key2", (void *)"value2", 7);
+  printf("Hashmap size: %d\n", map->size);
+
+  hashmap_insert(map, "key2", (void *)"value3", 7);
+  printf("Hashmap size: %d\n", map->size);
+
+  hashmap_insert(map, "key3", (void *)"random_access_memory", 21);
+  printf("Hashmap size: %d\n", map->size);
+
+  res = (char *)hashmap_get(map, "key");
+  printf("Key: key, value: %s\n", res);
+
+  res = (char *)hashmap_get(map, "key2");
+  printf("Key: key, value: %s\n", res);
+
+  res = (char *)hashmap_get(map, "key3");
+  printf("Key: key, value: %s\n", res);
+
+  hashmap_remove(map, "key");
+  printf("Hashmap size: %d\n", map->size);
+  
+  hashmap_remove(map, "key_not_in_map");
+  printf("Hashmap size: %d\n", map->size);
+
+  printf("Number of buckets before: %d\n", map->num_buckets);
+  hashmap_resize(map, 2000);
+  printf("Number of buckets after: %d\n", map->num_buckets);
+  
+  res = (char *)hashmap_get(map, "key2");
+  printf("Key: key, value: %s\n", res);
+
+  res = (char *)hashmap_get(map, "key3");
+  printf("Key: key, value: %s\n", res);
+
+  hashmap_delete(map);
+  printf("Deleted the hashmap!\n");
+
+  return 0;
+}
+
