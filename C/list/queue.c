b54deb82c073722ee02ee0b4ef2cad745a38e6b4
Finnur Torfason <finnurtorfa@gmail.com>
2014-05-23 22:06:27 +0200

    Add an empty spurce file for implementing a queue

---
+++ C/list/queue.c
@@ -0,0 +1,7 @@
+/*
+ * File: queue.c
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ */
+#include "queue.h"
+
---
+++ C/list/queue.h
@@ -0,0 +1,11 @@
+#pragma once
+/*
+ * File: queue.h
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.8.2
+ *
+ * A basic implementation of a queue
+ */
+#include <stdlib.h>
+#include <string.h>
+
