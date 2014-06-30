6c879f6feed255c4464c792c417ec5680610aabe
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-30 14:43:40 +0200

    Finish up the deque, which is basically a queue, but can be operated on on both ends

--- C/list/deque.c
+++ C/list/deque.c
@@ -5,3 +5,82 @@
  */
 #include "deque.h"
 
+void deque_init(struct deque_t *q) {
+  init_list_node(&q->node);
+}
+
+void deque_push(struct deque_t *q, void *data) {
+  struct deque_t *tmp;
+
+  tmp = (struct deque_t *)malloc(sizeof(struct deque_t));
+
+  tmp->data = data;
+
+  list_prepend(&(tmp->node), &(q->node));
+}
+
+void deque_push_left(struct deque_t *q, void *data) {
+  struct deque_t *tmp;
+
+  tmp = (struct deque_t *)malloc(sizeof(struct deque_t));
+
+  tmp->data = data;
+
+  list_append(&(tmp->node), &(q->node));
+}
+
+
+void deque_pop(struct deque_t *q) {
+  struct deque_t *tmp;
+
+  if ( deque_is_empty(q) ) {
+    return;
+  }
+
+  tmp = get_list_node(q->node.next, struct deque_t, node);
+
+  list_remove(q->node.next);
+  free(tmp);
+}
+
+void deque_pop_left(struct deque_t *q) {
+  struct deque_t *tmp;
+
+  if ( deque_is_empty(q) ) {
+    return;
+  }
+
+  tmp = get_list_node(q->node.prev, struct deque_t, node);
+
+  list_remove(q->node.prev);
+  free(tmp);
+}
+
+void * deque_peek(struct deque_t *q) {
+  struct deque_t *tmp;
+
+  if ( deque_is_empty(q) ) {
+    return NULL;
+  }
+
+  tmp = get_list_node(q->node.next, struct deque_t, node);
+
+  return tmp->data;
+}
+
+void * deque_peek_left(struct deque_t *q) {
+  struct deque_t *tmp;
+
+  if ( deque_is_empty(q) ) {
+    return NULL;
+  }
+
+  tmp = get_list_node(q->node.prev, struct deque_t, node);
+
+  return tmp->data;
+}
+
+bool deque_is_empty(struct deque_t *q) {
+  return q == get_list_node(q->node.next, struct deque_t, node);
+}
+
--- C/list/deque.h
+++ C/list/deque.h
@@ -17,3 +17,61 @@ struct deque_t {
   struct node_t node;
 };
 
+/**
+ * Initialize the deque
+ *
+ * @q: A deque to initialize
+ */
+void deque_init(struct deque_t *q);
+
+/**
+ * Add data to the back of the deque
+ *
+ * @q: A deque to add the data to
+ * @data: The data to add
+ */
+void deque_push(struct deque_t *q, void *data);
+
+/**
+ * Add data to the front of the deque
+ *
+ * @q: A deque to add the data to
+ * @data: The data to add
+ */
+void deque_push_left(struct deque_t *q, void *data);
+
+/**
+ * Remove data from the front of the deque
+ *
+ * @q; A deque to remove data from
+ */
+void deque_pop(struct deque_t *q);
+
+/**
+ * Remove data from the back of the deque
+ *
+ * @q; A deque to remove data from
+ */
+void deque_pop_left(struct deque_t *q);
+
+/**
+ * Return the first element in the deque, without removing it
+ *
+ * @q: The deque to peek at
+ */
+void * deque_peek(struct deque_t *q);
+
+/**
+ * Return the last element in the deque, without removing it
+ *
+ * @q: The deque to peek at
+ */
+void * deque_peek_left(struct deque_t *q);
+
+/**
+ * Returns wether or not the deque is empty
+ *
+ * @q: The deque to test
+ */
+bool deque_is_empty(struct deque_t *q);
+
