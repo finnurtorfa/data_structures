402b217cfb8386c2e9728b7f3bbc14f391565763
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-30 14:44:14 +0200

    Test for the deque, and Makefile update

---
+++ C/list/test_deque.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "deque.h"
+
+int main(int argc, char **argv) {
+  printf("Initializing deque!\n");
+
+  struct deque_t my_deque;
+  deque_init(&my_deque);
+
+  printf("Push-ing 1,2,3\n");
+
+  deque_push(&my_deque, (void *)1);
+  deque_push(&my_deque, (void *)2);
+  deque_push(&my_deque, (void *)3);
+
+  int *a = (int *)deque_peek(&my_deque);
+  printf("Taking a peek at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  printf("Push-ing left 1,2,3\n");
+
+  deque_push_left(&my_deque, (void *)1);
+  deque_push_left(&my_deque, (void *)2);
+  deque_push_left(&my_deque, (void *)3);
+
+  deque_pop(&my_deque);
+  printf("Pop-ing!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop_left(&my_deque);
+  printf("Pop-ing left!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop(&my_deque);
+  printf("Pop-ing!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop_left(&my_deque);
+  printf("Pop-ing left!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop(&my_deque);
+  printf("Pop-ing!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop_left(&my_deque);
+  printf("Pop-ing left!\n"); 
+
+  a = (int *)deque_peek(&my_deque);
+  printf("Taking a head at the front of the deque: %d\n", (int)a);
+
+  a = (int *)deque_peek_left(&my_deque);
+  printf("Taking a peek at the back of the deque: %d\n", (int)a);
+
+  deque_pop(&my_deque);
+
+  printf("Done!\n");
+
+  return 0;
+}
+
