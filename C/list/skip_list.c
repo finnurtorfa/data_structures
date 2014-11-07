3d436d447bdd16f65fb511af5b0c9a09dadf6d93
Finnur Torfason <finnurtorfa@gmail.com>
2014-11-07 21:48:25 +0100

    Add an empty skeleton for a skip list implementation

---
+++ C/list/skip_list.c
@@ -0,0 +1,8 @@
+/*
+ * File: skip_list.c
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.9.1
+ */
+
+#include "skip_list.h"
+
---
+++ C/list/skip_list.h
@@ -0,0 +1,11 @@
+#pragma once
+/*
+ * File: skip_list.h
+ * Author: Finnur Smári Torfason
+ * Compiler: GCC 4.9.1
+ * 
+ * An implementation of a skip list using linked lists.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
